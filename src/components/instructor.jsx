8578776a21b6b80bf7c05f3d1660f47eca8feb82
Zarsham Waleed <zarshamwaleedbutt@gmail.com>
2025-09-05 01:07:25 +0500

    url changed

--- src/components/instructor.jsx
+++ src/components/instructor.jsx
@@ -805,7 +805,7 @@ const handleCancelEdit = () => {
 
         {/* Edit Profile Modal */}
      {/* Edit Profile Modal */}
-// COMPLETE EDIT PROFILE MODAL JSX
+{/* // COMPLETE EDIT PROFILE MODAL JSX */}
 {editProfile && (
   <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
     <div className="bg-white rounded-lg shadow-xl max-w-md w-full max-h-[90vh] overflow-y-auto">
